e3a0b5aba04e520db22ea82819f028ae1debb1ed
liorbe <lior2612@gmail.com>
2017-01-08 23:48:37 +0200

    Add files via upload

---
+++ GUI/postToshow.cs
@@ -0,0 +1,9 @@
+﻿namespace GUI
+{
+    internal class posttoshow
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string content { get; set; }
+    }
+}
